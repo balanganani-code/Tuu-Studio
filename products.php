0c64d2097af72ed5b654a4d6d2a0a67c2a68130b
balanganani-code <balangananisadiki09@gmail.com>
2026-08-03 23:39:46 +0200

    Remove unused product sections and buttons for cleaner layout

--- products.php
+++ products.php
@@ -37,24 +37,6 @@ include 'config/db.php';
 
 
 
-    <section class="product-categories py-4">
-
-        <div class="product-buttons">
-
-            <div class="d-flex justify-content-center gap-3 flex-wrap">
-
-                <a href="#Custom-Illustrations"><button class="btn btn-outline-primary">Custom Illustrations</button></a>
-
-                <a href="#Branded-Merchandise"><button class="btn btn-outline-primary">Branded Merchandise</button></a>
-
-                <a href="#Novelty"><button class="btn btn-outline-primary">Novelty</button></a>
-
-
-            </div>
-
-        </div>
-
-    </section>
 
 
 
@@ -231,125 +213,6 @@ include 'config/db.php';
 
 
 
-<section id="Custom-Illustrations" class="featured-product py-5">
-    <h2>Custom Illustrations</h2>
-    <div class="row align-items-center">
-
-        <div class="row g-4">
-            <div class="illustration-product col-sm-6 col-md-4 col-lg-3">
-                <div class="pro-card">
-                    <img src="assets/images/Khutso.jpg" class="img-fluid">
-                    <h5>Ntwane</h5>
-                    <p>R350.00</p>
-                    <button class="btn btn-warning">
-                        <i class="bi bi-bag-heart"></i> </button>
-                </div>
-            </div>
-
-            <div class="illustration-product col-sm-6 col-md-4 col-lg-3">
-                <div class="pro-card">
-                    <img src="assets/images/Moipone.jpg" class="img-fluid">
-                    <h5>Xitsonga</h5>
-                    <p>R350.00</p>
-                    <button class="btn btn-warning">
-                        <i class="bi bi-bag-heart"></i> </button>
-                </div>
-            </div>
-
-
-            <div class="illustration-product col-sm-6 col-md-4 col-lg-3">
-                <div class="pro-card">
-                    <img src="assets/images/Rebaone.jpg" class="img-fluid">
-                    <h5>JoJo</h5>
-                    <p>R350.00</p>
-                    <button class="btn btn-warning">
-                        <i class="bi bi-bag-heart"></i> </button>
-                </div>
-            </div>
-
-            <div class="illustration-product col-sm-6 col-md-4 col-lg-3">
-                <div class="pro-card">
-                    <img src="assets/images/Rebecca.jpeg" class="img-fluid">
-                    <h5>Rekopantshitse</h5>
-                    <p>R350.00</p>
-                    <button class="btn btn-warning">
-                        <i class="bi bi-bag-heart"></i><br> </button>
-                    </button>
-                </div>
-            </div>
-
-            <div class="illustration-product col-sm-6 col-md-4 col-lg-3">
-                <div class="pro-card">
-                    <img src="assets/images/Bagwera-Baka.jpeg" class="img-fluid">
-                    <h5>Bagwera Baka</h5>
-                    <p>R350.00</p>
-                    <button class="btn btn-warning">
-                        <i class="bi bi-bag-heart"></i> </button>
-                </div>
-            </div>
-
-            <div class="illustration-product col-sm-6 col-md-4 col-lg-3">
-                <div class="pro-card">
-                    <img src="assets/images/Earth.jpeg" class="img-fluid">
-                    <h5>Earth</h5>
-                    <p>R350.00</p>
-                    <button class="btn btn-warning">
-                        <i class="bi bi-bag-heart"></i> </button>
-                </div>
-            </div>
-
-        </div>
-
-</section>
-
-
-<section id="Branded-Merchandise" class="featured-product py-5">
-    <h2>Branded Merchandise</h2>
-    <div class="row align-items-center">
-
-        <div class="row g-4">
-            <div class="illustration-product col-sm-6 col-md-4 col-lg-3">
-                <div class="pro-card">
-                    <img src="assets/images/T-shirt.png" class="img-fluid">
-                    <h5>Shangaan</h5>
-                    <p>R350.00</p>
-                    <button class="btn btn-warning">
-                        <i class="bi bi-bag-heart"></i> </button>
-                </div>
-            </div>
-
-
-
-
-
-        </div>
-
-</section>
-
-<section id="Novelty" class="featured-product py-5">
-    <h2>Novelty</h2>
-    <div class="row align-items-center">
-
-        <div class="row g-4">
-            <div class="illustration-product col-sm-6 col-md-4 col-lg-3">
-                <div class="pro-card">
-                    <img src="assets/images/Tote.png" class="img-fluid">
-                    <h5>Coming of Age -Tote Bag</h5>
-                    <p>R350.00</p>
-                    <button class="btn btn-warning">
-                        <i class="bi bi-bag-heart"></i> </button>
-                </div>
-            </div>
-
-        </div>
-
-    </div>
-
-
-
-
-
-    </main>
 
 
 
@@ -358,20 +221,4 @@ include 'config/db.php';
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    <?php include 'includes/footer.php'; ?>
+<?php include 'includes/footer.php'; ?>
